1327596227f65fa1b2518854d0a80947147965d3
AdrianAntunez <adriananve@gmail.com>
2015-05-27 14:58:43 +0200

    Remove debuging code; Add the real one

--- onion-path-finder/src/onion-path-finder.cpp
+++ onion-path-finder/src/onion-path-finder.cpp
@@ -16,23 +16,22 @@ Nodes nodes;
 
 int main(int argc, char *argv[])
 {
-//	if (argc != 7)
-//	{
-//		std::cout << "Usage: " << argv[0] << " <source> <destination> <time> <numPaths> <numNodes> <transmissionTime>" << std::endl;
-//		return 1;
-//	}
+	if (argc != 7)
+	{
+		std::cout << "Usage: " << argv[0] << " <source> <destination> <time> <numPaths> <numNodes> <transmissionTime>" << std::endl;
+		return 1;
+	}
 	nodes = Parser::getContactsFromTxt("../data/uab-busses-contacts-54000h-5n-sorted.txt");
 	OracleNetwork on = OracleNetwork(nodes);
 
-//	nodeid source = atoi(argv[1]);			// Source node
-//	nodeid destination = atoi(argv[2]);		// Destination node
-//	uint32_t startingTime = atoi(argv[3]);	// Starting time
-//	int numPaths = atoi(argv[4]);			// Number of paths to be retrieved
-//	int numNodes = atoi(argv[5]);			// Number of nodes for each path
-//	uint32_t transmissionTime = atoi(argv[6]);	// Time spent sending the message over nodes
+	nodeid source = atoi(argv[1]);			// Source node
+	nodeid destination = atoi(argv[2]);		// Destination node
+	uint32_t startingTime = atoi(argv[3]);	// Starting time
+	int numPaths = atoi(argv[4]);			// Number of paths to be retrieved
+	int numNodes = atoi(argv[5]);			// Number of nodes for each path
+	uint32_t transmissionTime = atoi(argv[6]);	// Time spent sending the message over nodes
 
-//	PathVector pv = on.getPaths(source, destination, startingTime, numPaths, numNodes, transmissionTime);
-	PathVector pv = on.getPaths(0, 4, 10, 3, 5, 20);
+	PathVector pv = on.getPaths(source, destination, startingTime, numPaths, numNodes, transmissionTime);
 
 	on.printPaths(pv);
 	return 0;
